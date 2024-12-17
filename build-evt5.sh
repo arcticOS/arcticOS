a9262dfc2e042064709d7c2161780f38193e8015
Johnny Stene <johnny@stene.xyz>
2024-12-17 11:44:58 -0700

    Clarify CPU on EVT5

--- build-evt5.sh
+++ build-evt5.sh
@@ -5,6 +5,7 @@ rm -rf build/evt5
 mkdir -p build/evt5
 export PICO_SDK_PATH=~/.pico_sdk
 export PICO_TOOLCHAIN_PATH=~/opt/arm/bin
+export PICO_PLATFORM=rp2350-arm-s
 cd build/evt5
 cmake -D_MODEL=EVT5 ../..
 BOARD=PICO_R1_3 RELEASE=1 make -j4
